a4d68aec9df31b8cf0ba2ed52ab6e6e5dc35b0aa
Komal <komalkhan0408@gmail.com>
2025-12-18 22:51:18 +0500

    Add WinForms student CRUD form

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+
+    {
+        private BindingSource bindingSource1 = new BindingSource();
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'universityDataSet.StudentTable' table. You can move, or remove it, as needed.
+            this.studentTableTableAdapter.Fill(this.universityDataSet.StudentTable);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+            GetData("select * from StudentTable");
+        }
+
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=University;Integrated Security=True;Pooling=False";
+                dataAdapter = new SqlDataAdapter(selectCommand, connectionString);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                DataTable table = new DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Exception ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+            //select * from Table where RollNo=3
+            GetData("select * from StudentTable where RollNo=" + textBox1.Text);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+            // insert into Table1 Values ( 6,'Aizaz',100,'TOK')  
+            GetData("Insert into StudentTable values(" + textBox2.Text + ", '" + textBox3.Text + "','" + textBox4.Text + "' " + ")");
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+            //Delete from Table1 where RollNo=4
+            GetData("Delete from  StudentTable where RollNo=" + textBox5.Text);
+
+        }
+    }
+}
